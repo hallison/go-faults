aaef343fde785751a35598d570857995e46ea63e
Hallison Batista <hallisonbatista@gmail.com>
2019-05-23 15:53:07 -0400

    First version

---
+++ README.md
@@ -0,0 +1,4 @@
+# Go-Faults - Error handle
+
+This is a small library to handle errors (a.k.a faults).
+
---
+++ faults.go
@@ -0,0 +1,98 @@
+// Copyright 2019 Hallison Batista. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+// Package faults provides a small errors handler.
+package faults
+
+import "fmt"
+
+// Type function that runs a block code.
+type BlockFunction func() error
+
+// Faults is a basic structure to handle errors.
+type Faults struct {
+	lastMessage string                   `json:"-"`
+	failures    map[string]error         `json:"errors"`
+	checklist   map[string]BlockFunction `json:"-"`
+	allowStack  bool                     `json:"-"`
+	locked      bool
+}
+
+// EnableStack if handle a stack of errors, this function enable this feature.
+func (f *Faults) EnableStack() *Faults {
+	f.allowStack = true
+	return f
+}
+
+// DisableStack if do not handle a stack of errors, this function disable this feature.
+func (f *Faults) DisableStack() *Faults {
+	f.allowStack = false
+	return f
+}
+
+// Reset set default values to faults handler.
+func (f *Faults) Reset() *Faults {
+	f.failures = make(map[string]error)
+	f.checklist = make(map[string]BlockFunction)
+	f.allowStack = false
+	f.locked = false
+	return f
+}
+
+// Check add a message and check if the block code should be runned testing if
+// has no failures or allow stack or unlocked.
+func (f *Faults) Check(message string, block BlockFunction) *Faults {
+	if f.IsEmpty() || f.allowStack || !f.locked {
+		f.lastMessage = message
+		if fail := block(); fail != nil {
+			f.failures[message] = fail
+		}
+	}
+
+	return f
+}
+
+// Add fail and message.
+func (f *Faults) Add(fail error, message string) *Faults {
+	f.failures[message] = fail
+	return f
+}
+
+// AddIf add fail and massage by condition.
+func (f *Faults) AddIf(condition bool, message string) *Faults {
+	if f.locked = condition; condition {
+		f.failures[message] = fmt.Errorf(message)
+	}
+	return f
+}
+
+// GetLast returns the last error.
+func (f *Faults) GetLast() error {
+	if f.lastMessage != "" {
+		return f.failures[f.lastMessage]
+	}
+	return nil
+}
+
+// GetAll returns all failures.
+func (f *Faults) GetAll() map[string]error {
+	return f.failures
+}
+
+// IsEmpty check if failures is empty.
+func (f *Faults) IsEmpty() bool {
+	return len(f.failures) == 0
+}
+
+// IsNotEmpty check if failures is not empty.
+func (f *Faults) IsNotEmpty() bool {
+	return !f.IsEmpty()
+}
+
+// New creates a new faults handler.
+func New() (f *Faults) {
+	f = &Faults{}
+	f.Reset()
+	return f
+}
